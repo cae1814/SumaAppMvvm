3891b40f573784e9024a5a19ba3809c54d7b713e
Antonio Banegas <cae1814@gmail.com>
2024-03-10 21:14:40 -0600

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using SumaAppMvvm.ViewModels;
+
+namespace SumaAppMvvm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ Models/Operaciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaAppMvvm.Models
+{
+    internal class Operaciones
+    {
+        public double Num1 { get; set; }
+
+        public double Num2 { get; set; }
+
+        public double TotalSuma() { 
+            return Num1 + Num2;
+        }
+
+        public double TotalSuma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
